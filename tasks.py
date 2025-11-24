f80c6e914a1ac21fbbee41efc5691427ff443f6e
Sterre <sterre.gesbert@gmail.com>
2025-11-24 22:36:42 +0100

    3.1415926535897932384626433832795028841971693993751058230

--- tasks.py
+++ tasks.py
@@ -95,7 +95,7 @@ print(word_frequencies)
 # -----------------------------------------------
 def token_counts(string: str, k: int = 1) -> dict:
     tokens = [word.strip(strmod.punctuation).lower() for word in string.split() if word.strip(strmod.punctuation)]
-    freq = {word: tokens.count(word) for word in tokens if tokens.count(word) > k}
+    freq = {word: tokens.count(word) for word in set(tokens) if tokens.count(word) > k}
     return freq
 
 # test:
